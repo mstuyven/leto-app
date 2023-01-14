c10e896cecb9c729e407a65a053d5ec895593653
mstuyven <mirostuyven@gmail.com>
2023-01-14 17:02:06 +0100

    Use custom http client with increased timeout

--- app/src/main/java/com/mirostuyven/leto/network/LetoApiService.kt
+++ app/src/main/java/com/mirostuyven/leto/network/LetoApiService.kt
@@ -5,10 +5,13 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.concurrent.TimeUnit
+
 
 private const val BASE_URL = "https://leto.onrender.com/"
 
@@ -16,9 +19,15 @@ private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+private val okHttpClient = OkHttpClient.Builder()
+    .readTimeout(60, TimeUnit.SECONDS)
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
+    .client(okHttpClient)
     .build()
 
 @Parcelize
